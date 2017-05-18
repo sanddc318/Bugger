3efb5cb5554da843c41861bd0228239da45fa2e1
sanddc318 <sanddc318@gmail.com>
2017-05-18 14:54:02 -0400

    Configured firebase database to be accessible.

--- app/core/service/firebase-config.service.ts
+++ app/core/service/firebase-config.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+require('firebase/database');
 
 import { FIREBASE_CONFIG } from '../constant/constants';
 
 
 @Injectable()
 export class FirebaseConfigService {
+  private database: firebase.database.Database;
+
   constructor() {
     this.configureApp();
+    this.configureDatabase();
   }
 
   configureApp() {
     firebase.initializeApp(FIREBASE_CONFIG);
+  }
+
+  configureDatabase() {
+    this.database = firebase.database();
   }
 }
